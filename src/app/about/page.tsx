e71a3079c4757914bf621bc7e08e0d8d14b014e5
Tim Cheung <timshingyu@gmail.com>
2022-11-20 18:55:47 +0800

    test: added playwright for e2e test

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">About</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">About</h1>
+      <Link href="/">Home</Link>
+    </>
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react()],
@@ -7,6 +7,7 @@ export default defineConfig({
     coverage: {
       reporter: ['text', 'json', 'html'],
     },
+    exclude: [...configDefaults.exclude, 'src/test/e2e'],
     environment: 'happy-dom',
   },
 })
