ce5fe2152f355b51afb04dc2ad1f8856024c966f
Tim Cheung <timshingyu@gmail.com>
2022-11-20 18:55:47 +0800

    test: added playwright for e2e test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Home</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Home</h1>
+      <Link href="/about">About</Link>
+    </>
+  )
 }
